a3d2153c76de4802bacde548b9045faae84c164e
ResidentTenko <fidelojimba@gmail.com>
2022-09-14 20:20:46 -0400

    First Session

---
+++ app/src/main/java/com/example/wishlist/WIshList.kt
@@ -0,0 +1,5 @@
+package com.example.wishlist
+
+// create the Wish List class - all wish list objects will have the three properties listed
+// use the kotlin primary constructor
+class WIshList (val itemName: String, val itemPrice: Int, val urlLink: String,){}
